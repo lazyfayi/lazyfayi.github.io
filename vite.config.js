5429d8789ba6088836cece65e49a00281e89a5d3
R. Rifai Arsandi <80803750+lazyfayi@users.noreply.github.com>
2023-04-18 14:43:32 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import tailwindConfig from './tailwind.config.mjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // If you are deploying to https://<USERNAME>.github.io/, set base to '/'.
-  // If you are deploying to https://<USERNAME>.github.io/<REPO>/, for example your repository is at https://github.com/<USERNAME>/<REPO>, then set base to '/<REPO>/'.
+  // If you are deploying to https://lazyfayi.github.io/, set base to '/'.
+  // If you are deploying to https://lazyfayi.github.io/lazyfayi>/, for example your repository is at https://github.com/<USERNAME>/<REPO>, then set base to '/<REPO>/'.
   base: '/gitprofile/',
   plugins: [react()],
   css: {
